12fbc2478f780763c5842bd148d20838478f1b75
rvdxk <radek.szczot@gmail.com>
2024-04-14 08:33:57 +0200

    create project

---
+++ src/test/java/io/github/rvdxk/sms/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rvdxk.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
